0656106ae3944c8bb28dad507f2892d3e35f2b35
n7best <n7best@gmail.com>
2017-07-07 15:43:33 -0400

    adding translatejson

--- src/translators/yandex.js
+++ src/translators/yandex.js
@@ -2,6 +2,18 @@
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
 
+/* eslint-disable */
+function iterate(obj, cb, write) {
+  for (const property in obj) {
+    if (obj.hasOwnProperty(property)) {
+      if (typeof obj[property] === 'object') { iterate(obj[property], cb, write); } else if (write) {
+        obj[property] = cb();
+      } else { cb(obj[property]); }
+    }
+  }
+}
+/* eslint-enable */
+
 class YandexTranslator {
   constructor(apiKey) {
     const API_URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate';
@@ -10,6 +22,35 @@ class YandexTranslator {
     this.apiURL = `${API_URL}?key=${apiKey}`;
   }
 
+  translateJSON(json, src, target, callback) {
+    const data = [];
+    iterate(json, (text) => {
+      const regMustache = /\{\{(.*?)\}\}/g;
+
+      if (text.match(regMustache)) {
+        data.push(text.replace(regMustache, (match, p1) => `NO_TRANSLATION_${p1.trim()}`));
+      } else {
+        data.push(text);
+      }
+    });
+
+    this.translate(data, src, target, (translations) => {
+      const newLangData = JSON.parse(JSON.stringify(json));
+      let index = 0;
+
+      iterate(newLangData, () => {
+        let trans = translations[index];
+        index += 1;
+
+        if (trans.indexOf('NO_TRANSLATION_') > -1) {
+          trans = trans.replace(/NO_TRANSLATION_(\w+)/g, (match, p1) => `{{ ${p1} }}`);
+        }
+        return trans;
+      }, true);
+
+      callback(newLangData);
+    });
+  }
 
   translate(texts, source, target, callback) {
     const reqURL = `${this.apiURL}&lang=${target.slice(0, 2)}`;
